018da142b90bd1543439a1c94d2f65062831c4c9
Jamie Kennedy <jamiekennedy97@gmail.com>
2022-08-10 20:54:30 +0100

    Added board object with initialisation

---
+++ WaveFunctionCollapseSudokuSolver/SudokuSolver/Objects/Board.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Objects
+{
+    public class Board
+    {
+        public int BoardSize { get; set; } // BoardSize n = n x n grid
+        public Cell[,] Cells { get; set; } // 2D array of cells
+
+        public Board(int boardSize)
+        {
+            this.BoardSize = boardSize;
+            this.Cells = InitCells(boardSize);
+        }
+
+        private static Cell[,] InitCells(int boardSize)
+        {
+            Cell[,] cells = new Cell[boardSize, boardSize];
+
+            for(int i = 0; i < boardSize; i++)
+            {
+                for(int j = 0; j < boardSize; j++)
+                {
+                    cells[i, j] = new Cell((i, j), boardSize);
+                }
+            }
+
+            return cells;
+        }
+    }
+}
